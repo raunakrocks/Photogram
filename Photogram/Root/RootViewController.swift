0929d5736a4ce5afc278693d8c7396fa71bd968e
Raunak Talwar <raunaktalwar00@gmail.com>
2021-01-06 03:54:26 +0800

    add collection view and search bar UI code.

--- Photogram/Root/RootViewController.swift
+++ Photogram/Root/RootViewController.swift
@@ -7,15 +7,34 @@
 
 import UIKit
 
-final class RootViewController: UIViewController {
+final class RootViewController: UIViewController, UICollectionViewDataSource {
 
     private let viewModel: RootViewModel
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemRed
-    }
-
+    private lazy var containerView: UIView = {
+        let view = UIView().withAutoLayout()
+        return view
+    }()
+    
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar().withAutoLayout()
+        let searchImage = UIImage(named: "search")!
+        searchBar.setLeftImage(searchImage, with: 8, tintColor: UIColor.systemBlue)
+        searchBar.textField?.placeholder = "Search Here!!"
+        return searchBar
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: 64, height: 64)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout).withAutoLayout()
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+    
     init(viewModel: RootViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -25,5 +44,53 @@ final class RootViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupViews()
+        setupContraints()
+    }
+    
+    //MARK: - Private Methods
+    
+    private func setupViews() {
+        view.addSubview(containerView)
+        containerView.addSubview(searchBar)
+        containerView.addSubview(collectionView)
+    }
+    
+    private func setupContraints() {
+        var contraints = [NSLayoutConstraint]()
+        let safeAreaLayoutGuide = view.safeAreaLayoutGuide
+        
+        contraints.append(containerView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16.0))
+        contraints.append(containerView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16.0))
+        contraints.append(containerView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16.0))
+        contraints.append(containerView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -16.0))
+        
+        contraints.append(searchBar.leadingAnchor.constraint(equalTo: containerView.leadingAnchor))
+        contraints.append(searchBar.trailingAnchor.constraint(equalTo: containerView.trailingAnchor))
+        contraints.append(searchBar.topAnchor.constraint(equalTo: containerView.topAnchor))
+        contraints.append(searchBar.heightAnchor.constraint(equalToConstant: 24.0))
+        
+        contraints.append(collectionView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor))
+        contraints.append(collectionView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor))
+        contraints.append(collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor))
+        contraints.append(collectionView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor))
+        
+        NSLayoutConstraint.activate(contraints)
+    }
+    
+    //MARK: - UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 13000
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath)
+        myCell.backgroundColor = UIColor.blue
+        return myCell
+    }
 }
 
